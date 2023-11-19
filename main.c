38d53c5c628973b7e6cd686ef8aba92417c5c54b
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2023-11-19 12:56:10 +0100

    01 zmiana metody policz

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+int Policz(int a, int b){
+    return a+b;
+}
+
 int main() {
     printf("P1v2!\n");
+    printf( "W pierwszym branchu; 2+3=", Policz(2,3));
     return 0;
 }
