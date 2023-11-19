bab2df11d4c03e6145ed5e2575c766b9fc932014
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2023-11-19 12:50:29 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/abens.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/abens.dir/main.c.o"
+  "abens"
+  "abens.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/abens.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
