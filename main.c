4dda854575853896c7dd7117b7c6abec600ec4c2
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2023-11-19 13:06:33 +0100

    01 zmiana mozenia metody policz

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 int Policz(int a, int b){
-    return a+b;
+    return a*b;
 }
 
 int main() {
     printf("P1v2!\n");
-    printf( "W pierwszym branchu: 2+3= %d", Policz(2,3));
-    printf("commit 2");
+    printf( "W pierwszym branchu: 2*3= %d", Policz(2,3));
     return 0;
 }
